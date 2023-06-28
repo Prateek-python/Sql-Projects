b27a03c098c3bdf7edc996ad447225b62cab00ea
Prateek-python <91688624+Prateek-python@users.noreply.github.com>
2023-06-28 12:38:15 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
-console.log("My name is Prateek Wayne");
+console.log("My name");
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
+console.log(Car.name+" " +Car.year);
